edd798b2e113ca0d53dabd714968cc933e97c86d
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-13 19:28:54 +0600

    a class created named Account and initialised with name, email, address and account type and the balance will be zero at the beginning and account number will be generated automatically from 21

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class Account(ABC):
-    def __init__(self) -> None:
+    accounts = []
+
+    def __init__(self, name, email, address, account_type) -> None:
         super().__init__()
+        self.name = name
+        self.email = email
+        self.address = address
+        self.account_type = account_type
+        self.balance = 0
+        self.account_no = len(self.accounts) + 21
+        Account.accounts.append(self)
