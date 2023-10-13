f12ea37c1349a1140ca19873290fad223a3cc40b
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-13 19:37:39 +0600

    withdraw added successfull

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ class Account(ABC):
         self.balance = 0
         self.account_no = len(self.accounts) + 21
         Account.accounts.append(self)
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdraw: ${amount}. New Balance: ${self.balance}")
+        elif amount > self.balance:
+            print(f"Withdrawal amount exceeded")
+        else:
+            print(f"Invalid amount request. Please Try again!")
